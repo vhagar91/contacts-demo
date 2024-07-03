fe49d0008a7387280dd6444ac8d6623f114c3f4f
oviera <oviera@vmware.com>
2024-07-03 20:12:07 +0300

    Default to 500

--- contacts-backend/src/modules/contacts/services/contact.business.service.ts
+++ contacts-backend/src/modules/contacts/services/contact.business.service.ts
@@ -70,7 +70,7 @@ export class ContactsBusinessService {
             return await this.contactsService.registerContact(contact, requestedBy);
         } catch (error) {
             this.logger.error(`Failed to import or update contact in Freshdesk: ${error.message}`);
-            throw new HttpException('Failed to import or update contact in Freshdesk', error.status);
+            throw new HttpException('Failed to import or update contact in Freshdesk', error.status || 500);
         }
     }
 
